150706f2e59622efaa22f4d0a9c53641075ec372
DinaraDervel <dinaradervel@gmail.com>
2023-10-30 10:59:55 -0600

    add loader, add renderError function, try catch finally blocks

--- index.js
+++ index.js
@@ -1,54 +1,75 @@
 const URL = 'https://swapi.dev/api/';//root resource
 const searchResult = document.querySelector('#searchResult');//parent element for search result
 const searchError = document.querySelector('#searchError');//parent element for search error message
+const loader = document.querySelector('.loader');
 //object for searched category and number
 const requestURL = {
     category: '',
-    number: 0,
+    number: '',
 };
 
-// function for rendering search result in DOM inside parent element
+// functions for rendering search result and errors in DOM inside parent element
 const render = (element, ParentElement) => {
     if (!element) ParentElement.innerHTML = ``;
-    else if (element instanceof Error) ParentElement.innerHTML = `<h3>Error: ${element.message}</h3>`;
     else if (requestURL.category === 'films') ParentElement.innerHTML = `<h3>Title: ${element.title}</h3>`;
     else ParentElement.innerHTML = `<h3>Name: ${element.name}</h3>`;
+};
+const renderError = (error, ParentElement) => {
+    if (!error) ParentElement.innerHTML = ``;
+    else if (error instanceof Error) ParentElement.innerHTML = `<h3 style="color:red;">Error: Server is not available. ${error.message}</h3>`;
+    else if (error === 404) ParentElement.innerHTML = `<h3 style="color:red;">Error: 404 Not Found</h3>`;
+    else ParentElement.innerHTML = `<h3 style="color:red;">Error: ${error}</h3>`;
 };
 
 //function for GET request and rendering result or error message
 const getRequest = (url) => {
-    fetch(url)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(response.status);
-            }
-            return response.json();
-        })
-        .then(data => {
-            render(data, searchResult);
-            render('', searchError);
-        })
-        .catch(err => {
-            render(err, searchError);
-            render('', searchResult)
-        })
+    try {
+        renderError('', searchError);
+        render('', searchResult);
+        loader.classList.remove('hidden');
+        fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    return Promise.reject(response.status);
+                }
+                return response.json();
+            })
+            .then(data => {
+                render(data, searchResult);
+                renderError('', searchError);
+                loader.classList.add('hidden');
+            },
+                reject => {
+                    renderError(reject, searchError);
+                    render('', searchResult);
+                    loader.classList.add('hidden');
+
+                });
+    }
+    catch (err) {
+        renderError(err, searchError);
+        render('', searchResult);
+    }
+    finally {
+        console.log('Request sent');
+    }
 };
 
 //read category value from input and assign it to object property
 const searchCategory = document.getElementById('objectCategory');
-const onCategoryChange = searchCategory.addEventListener('change', () => {
+searchCategory.addEventListener('change', () => {
     requestURL.category = searchCategory.value;
 });
 
 //read number value from input and assign it to object property
 const searchNumber = document.getElementById('objectNumber');
-const onNumberChange = searchNumber.addEventListener('change', () => {
+searchNumber.addEventListener('change', () => {
     requestURL.number = searchNumber.value;
 });
 
 //create final url for request and execute request function
 const searchButton = document.getElementById('searchButton');
-const searchObject = searchButton.addEventListener('click', () => {
+searchButton.addEventListener('click', () => {
     const searchURL = URL + requestURL.category + '/' + requestURL.number + '/';
     getRequest(searchURL);
 });
